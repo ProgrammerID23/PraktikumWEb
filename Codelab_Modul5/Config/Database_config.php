76bec049be87d9cbdc7c893185340cd3073fa5eb
ProgrammerID23 <76148355+ProgrammerID23@users.noreply.github.com>
2023-11-30 11:26:03 +0700

    Add files via upload

---
+++ Codelab_Modul5/Config/Database_config.php
@@ -0,0 +1,11 @@
+<?php
+
+ namespace app\Config;
+
+ class DatabaseConfig{
+    public $host = "localhost";
+    public $user = "root";
+    public $pass = "";
+    public $database_name = "praktikum_codelab";
+    public $port = 3306;
+ }
